da1e6c31e4068d9e0ac34d213cf856f39acb9cee
jimous <jimouspeng@xiaoe-tech.com>
2022-02-25 18:26:31 +0800

    log this

--- jimous-vue/src/App.vue
+++ jimous-vue/src/App.vue
@@ -2,35 +2,45 @@
  * @Date: 2022-02-11 10:25:59
  * @LastEditors: jimouspeng
  * @Description: 描述文件内容
- * @LastEditTime: 2022-02-11 10:38:01
+ * @LastEditTime: 2022-02-25 10:16:10
  * @FilePath: \vue\jimous-vue\src\App.vue
 -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <show-model />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+        <show-model />
+        <model />
+    </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-
+import Model from './components/model.vue'
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  }
+    name: 'App',
+    components: {
+        Model,
+        // HelloWorld
+    },
+    data() {
+        return {
+            name: 'jimous',
+        }
+    },
+    created() {
+        // console.log(this.name, this)
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ jimous-vue/src/components/model.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Date: 2022-02-25 10:15:15
+ * @LastEditors: jimouspeng
+ * @Description: 描述文件内容
+ * @LastEditTime: 2022-02-25 10:18:46
+ * @FilePath: \vue\jimous-vue\src\components\model.vue
+-->
+<template>
+    <div>{{ name }}</div>
+</template>
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            default: 'jimous',
+        },
+    },
+    data() {
+        return {
+            hh: 1212,
+        }
+    },
+    created() {
+        console.log(this)
+    },
+}
+</script>
+<style lang="scss" scoped></style>
