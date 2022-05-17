880ff259a9fac8dcd0ed2a9481258330dd09c2f0
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-05-17 15:00:40 +0800

    fix

--- test.mjs
+++ test.mjs
@@ -1,4 +1,12 @@
+/*
+ * @Author: jimouspeng
+ * @Date: 2022-05-07 14:11:59
+ * @Description:
+ * @FilePath: \vue\test.mjs
+ */
 /** https://www.npmjs.com/package/path-to-regexp */
 import { pathToRegexp } from 'path-to-regexp';
 
-console.log(pathToRegexp('/foo/:id', []));
+const pathList = [];
+const regCtx = pathToRegexp('/foo/:id', pathList, {});
+console.log(regCtx, pathList);
